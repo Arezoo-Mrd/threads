01142ef9797e23acbcc6dc4a5325d8720700e2c6
Arezoo Mordai <moradi.arezoo1992@gmail.com>
2023-11-28 15:15:29 +0330

    learning some syntax and store

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <MainLayout>
+        <div id="IndexPage" class="w-full overflow-auto">
+            <div class="mx-auto max-w-[500px] overflow-hidden">
+                <div id="Posts" class="px-4 max-w-[600px] mx-auto" v-if="isPosts">
+                    <div v-for="post in userStore.posts" :key="post" class="">
+                        {{ post }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </MainLayout>
+</template>
+
+<script setup>
+import { useUserStore } from '~/stores/user';
+import MainLayout from '../layouts/MainLayout.vue';
+const userStore = useUserStore()
+// const user = useSupabaseUser() 
+const isPosts = true
+</script>
